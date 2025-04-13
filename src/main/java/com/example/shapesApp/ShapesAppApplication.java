c7bd3c9baa056a8a2629250279752703c46813ac
IgnBys <ignatbyshniov@gmail.com>
2025-04-13 17:06:30 +0200

    initial commit

---
+++ src/main/java/com/example/shapesApp/ShapesAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.shapesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShapesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shapesApp/ShapesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shapesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShapesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
